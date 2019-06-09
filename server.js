8db29c72cc8d7304ce92f487fc22ce6301867db8
MaciejKut <maciej.kutzmann@gmail.com>
2019-06-09 13:43:24 +0200

    Update connection address into mongodb://localhost:27017/test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://cluster0-wju01.mongodb.net/test', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
 
 //new user Schema
 const userSchema = new Schema({
