b3d831882833b7ab27e7163bb3a57a2da63e966a
MaciejKut <maciej.kutzmann@gmail.com>
2019-06-09 13:31:34 +0200

    change connect into mongodb+srv://cluster0-wju01.mongodb.net/test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://cluster0-wju01.mongodb.net/test', { useNewUrlParser: true });
 
 //new user Schema
 const userSchema = new Schema({
